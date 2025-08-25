6e64f1023c725febcdd95502eafd26a71bac7e06
Shourya wadhwa <71631493+CoderLogy@users.noreply.github.com>
2025-08-25 03:59:13 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { PostHogProvider } from 'posthog-js/react'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
